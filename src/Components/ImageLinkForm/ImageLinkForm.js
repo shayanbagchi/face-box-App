3f95fa8d2e1d862a1fe40580e66ceb70df8034d1
shayanbagchi <bagchishayan.9@gmail.com.com>
2018-11-01 21:52:57 +0530

    tweeking css

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -4,16 +4,16 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({onInputChange,onSubmit}) => {
 	return (
 		<div className='f3'>
-		<p>
-		{'Detect Faces with One click !'}
-		</p>
-		<div className='center'>
-		<div className='form center pa4 br3 shadow-5' >
-		<input className='f4 pa2 w-70 center' type='text'
-		onChange={onInputChange}/>
-		<button className='w-30 grow f4 link ph3 dib pv2 white bg-light-purple' onClick={onSubmit}>Detect</button>
-		</div>
-		</div>
+			<p>
+				{'Detect Faces with One click !'}
+			</p>
+			<div className='center'>
+				<div className='form center pa4 br3 shadow-5' >
+					<input className='f4 pv2  w-70 ' type='text'
+						onChange={onInputChange}/>
+					<button className='buttoncolor w-30 grow f4 link pr3 dib pv2 white pa0 ma0' onClick={onSubmit}>Detect</button>
+				</div>
+			</div>
 		</div>
 		);
 }
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -45,7 +45,7 @@ class Register extends React.Component {
 		<main className="pa4 black-80">
 		<div className="measure">
 		<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-		<legend className="f4 fw6 ph0 mh0">Register</legend>
+		<legend className="f2 fw6 ph0 mh0">Register</legend>
 		<div className="mt3">
 		<label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
 		<input
