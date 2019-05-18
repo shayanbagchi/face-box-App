a75b2208eee75fbf5564edf8ddcb228b687c6c4c
shayanbagchi <bagchishayan.9@gmail.com.com>
2019-05-18 21:31:45 +0530

    Update 0.1.1

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -19,7 +19,7 @@ class Register extends React.Component {
 		this.setState({password: event.target.value})
 	}
 	onSubmitSignIn = () =>{
-		fetch('https://face-box-server.herokuapp.com//register',{
+		fetch('https://face-box-server.herokuapp.com/register',{
 			method: 'post',
 			headers: {'Content-Type':'application/json'},
 			body:JSON.stringify({
--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
 	}
 
 	onSubmitSignIn = () =>{
-		fetch('https://face-box-server.herokuapp.com//signin',{
+		fetch('https://face-box-server.herokuapp.com/signin',{
 			method: 'post',
 			headers: {'Content-Type':'application/json'},
 			body:JSON.stringify({
